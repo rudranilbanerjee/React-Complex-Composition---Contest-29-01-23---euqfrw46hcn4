a4fa20bdd158201f73dda64c3a8161c3afdd2113
Rudranil Banerjee <105772744+rudranilbanerjee@users.noreply.github.com>
2023-01-29 10:21:41 +0530

    Add files via upload

---
+++ src/components/Fruits.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TypesOfFruits from './TypesOfFruits'
+
+import React, { Component } from 'react'
+
+export default class Fruits extends Component {
+  render() {
+    return (
+      <TypesOfFruits/>
+    )
+  }
+}
+
---
+++ src/components/TypesOfFruits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import React, { Component } from 'react'
+
+export default class TypesOfFruits extends Component {
+  render() {
+    return (
+      <div>TypesOfFruits</div>
+    )
+  }
+}
+
